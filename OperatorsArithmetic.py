a8dead597af805739a53d72a0cfcaa8757dcb8eb
José Ignacio Elzo <elzo.merino@gmail.com>
2020-08-19 10:17:27 -0400

    type of variables and operations

---
+++ OperatorsArithmetic.py
@@ -0,0 +1,35 @@
+""" (int) Integers = enteros 
+(float)Float = flotantes, decimales 
+(complex )complex = numeros complejos y largos 
+(bool)bolean = 0,1 true or false 
+(str)strings = texto  """
+
+#OPERATIONS
+
+#SUM
+suma= 1+ 1.5 
+print (suma)
+
+#SUBSTRACTION
+sub= 1 - 1.5 
+print (sub)
+
+#MULTIPLICATION
+mult= 1 * 2
+print (mult)
+
+#DIVISION
+suma= 1+ 1.5 
+print (suma)
+
+#MODULE (#Prints the how many times a number can be divided)
+md = 10 % 3
+print (md)
+
+#EXPONENTIAL
+ex = 2**5
+print (ex)
+
+#with the function type() we can print the type of the variable
+asd = "this variable it's a type string"
+print (type(asd))
