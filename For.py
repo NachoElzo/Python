c60a511c14f6f575849112a5f8f31f829f3d0ef9
Elzo <jelzo@comscore.com>
2020-08-20 14:47:12 -0400

    For

---
+++ For.py
@@ -0,0 +1,9 @@
+#WE HAVE 2 TYPES OF CYCLES DETERMINED AND INDETERMINATE 
+#DETERMINATED CYCLE WE KNOW HOW MANY TIMES THE BUCKLE WILL BE EXECUTED
+#WE DONT KNOW WHEN THE CYCLE END, UNTIL THE CODITIONS GETS FULLFILLED (FOR)
+
+#DETERMINATED BUCLE EXAMPLE
+
+for i in ["one","two","Three"]: #declaration of the bucle
+#i its the variable name) in (can be variables, list, tuples,dictionaries)
+    print (i) #body of the bucle
